ba0629cc03077866943b17b22696154985f03fef
mingaangshin-byte <mingaang.shin@alumni.mondragon.edu>
2026-09-17 11:59:30 +0900

    feat: make the main fish roam across the screen

--- components/PixelFish.tsx
+++ components/PixelFish.tsx
@@ -1,2 +1,11 @@
 import Link from 'next/link';
-export default function PixelFish(){return <Link href="/about" className="fish-link" aria-label="Remo 소개로 이동"><span className="fish"/><span className="click-hint">CLICK!</span></Link>}
+
+export default function PixelFish(){
+  return (
+    <Link href="/about" className="fish-link" aria-label="Remo 소개로 이동">
+      <span className="fish" aria-hidden="true">
+        <span className="fish-eye" />
+      </span>
+    </Link>
+  );
+}
